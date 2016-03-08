198a3cfc791a1132d30a635e54c149871eb69f62
tas2580 <tobi.schaefer@gmail.com>
2016-03-08 03:58:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<img src="https://tas2580.net/downloads/phpbb-lasttopiclink/image.png" width="600" height="80" alt="phpBB 3.1 - AJAX Userinfo" />
+
+This is an extension for the phpBB forums software. You need an instalation of phpBB 3.1.x to use this.
+
+DESCRIPTION
+-------
+Changes the URLs of forums and topics too more SEO friendly URLs with the title of the forums and topics in it. This
+extension is held as simple as possible, it contains no ACP, you can just install it and use the SEO friendly URLs.
+
+
+INSTALATION
+----------
+To install this extension download it from here and upload the files in your forum under <b>/ext/tas2580/lasttopiclink</b>.
+After that go to the Admin panel of your forum and navigate in to Customise -> Extension Management -> Extensions. Search this extension in the list of extensions and click on Enable.
+
+SUPPORT
+-------
+You can get support for this extension on <a href="https://www.phpbb.com/community/viewtopic.php?f=456&t=2288486">phpbb.com</a>
+or in german on <a href="https://www.phpbb.de/community/viewtopic.php?f=149&t=233380">phpbb.de</a>. For more informations look at
+<a href="https://tas2580.net/downloads/phpbb-lasttopiclink/">my Website</a>
+
+LICENSE
+-------
+<a href="http://opensource.org/licenses/gpl-2.0.php">GNU General Public License v2</a>
+
+[![Build Status](https://travis-ci.org/tas2580/seourls.svg?branch=master)](https://travis-ci.org/seourls)
---
+++ event/listener.php
@@ -0,0 +1,260 @@
+<?php
+
+/**
+ *
+ * @package phpBB Extension - tas2580 lasttopiclink
+ * @copyright (c) 2016 tas2580 (https://tas2580.net)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace tas2580\lasttopiclink\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Event listener
+ */
+class listener implements EventSubscriberInterface
+{
+
+	/** @var \phpbb\auth\auth */
+	protected $auth;
+
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\path_helper */
+	protected $path_helper;
+
+	/** @var string phpbb_root_path */
+	protected $phpbb_root_path;
+
+	/** @var string php_ext */
+	protected $php_ext;
+
+	/**
+	 * Constructor
+	 *
+	 * @param \phpbb\auth\auth				auth				Authentication object
+	 * @param \phpbb\config\config			$config				Config Object
+	 * @param \phpbb\template\template		$template			Template object
+	 * @param \phpbb\request\request			$request			Request object
+	 * @param \phpbb\user					$user				User Object
+	 * @param \phpbb\path_helper				$path_helper		Controller helper object
+	 * @param string                         $phpbb_root_path	phpbb_root_path
+	 * @access public
+	 */
+	public function __construct(\phpbb\auth\auth $auth, \phpbb\config\config $config, \phpbb\request\request $request, \phpbb\path_helper $path_helper, $phpbb_root_path, $php_ext)
+	{
+		$this->auth = $auth;
+		$this->config = $config;
+		$this->request = $request;
+		$this->path_helper = $path_helper;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->php_ext =$php_ext;
+	}
+
+	/**
+	 * Assign functions defined in this class to event listeners in the core
+	 *
+	 * @return array
+	 * @static
+	 * @access public
+	 */
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.append_sid'						=> 'append_sid',
+			'core.display_forums_modify_sql'			=> 'display_forums_modify_sql',
+			'core.display_forums_modify_template_vars'	=> 'display_forums_modify_template_vars',
+			'core.display_forums_modify_forum_rows'		=> 'display_forums_modify_forum_rows',
+			'core.display_forums_modify_sql'			=> 'display_forums_modify_sql',
+			'core.viewforum_modify_topicrow'			=> 'viewforum_modify_topicrow',
+			'core.search_modify_tpl_ary'				=> 'search_modify_tpl_ary',
+			'core.viewtopic_modify_post_row'			=> 'viewtopic_modify_post_row',
+
+
+		);
+	}
+
+	/**
+	 * Rewrite forum index from index.php to forum.html
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function append_sid($event)
+	{
+		if ($event['url'] == $this->path_helper->update_web_root_path($this->phpbb_root_path . 'index.php') && empty($event['params']))
+		{
+			//$event['append_sid_overwrite'] = $this->path_helper->update_web_root_path($this->phpbb_root_path . 'forum.html');
+			$event['append_sid_overwrite'] = $this->path_helper->update_web_root_path($this->phpbb_root_path);
+		}
+	}
+
+
+	/**
+	 * Get informations for the last post from Database
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function display_forums_modify_sql($event)
+	{
+		$sql_array = $event['sql_ary'];
+		$sql_array['LEFT_JOIN'][] = array(
+			'FROM' => array(TOPICS_TABLE => 't'),
+			'ON' => "f.forum_last_post_id = t.topic_last_post_id"
+		);
+		$sql_array['SELECT'] .= ', t.topic_title, t.topic_id, t.topic_posts_approved, t.topic_posts_unapproved, t.topic_posts_softdeleted';
+		$event['sql_ary'] = $sql_array;
+	}
+
+	/**
+	 * Store informations for the last post in forum_rows array
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function display_forums_modify_forum_rows($event)
+	{
+		$forum_rows = $event['forum_rows'];
+		if ($event['row']['forum_last_post_time'] == $forum_rows[$event['parent_id']]['forum_last_post_time'])
+		{
+			$forum_rows[$event['parent_id']]['topic_id_last_post'] =$event['row']['topic_id'];
+			$event['forum_rows'] = $forum_rows;
+		}
+	}
+
+
+	/**
+	 * Rewrite links to forums and subforums in forum index
+	 * also correct the path of the forum images if we are in a forum
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function display_forums_modify_template_vars($event)
+	{
+		$replies = $this->get_count('topic_posts', $event['row'], $event['row']['forum_id']) - 1;
+		$url = $this->generate_topic_link($event['row']['forum_id_last_post'], $event['row']['topic_id_last_post']);
+
+		$forum_row = $event['forum_row'];
+		$forum_row['U_LAST_POST'] = $this->generate_lastpost($replies, $url) . '#p' . $event['row']['forum_last_post_id'];
+		$event['forum_row'] = $forum_row;
+	}
+
+
+
+	/**
+	 * Rewrite links in the search result
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function search_modify_tpl_ary($event)
+	{
+		$replies = $this->get_count('topic_posts', $event['row'], $event['row']['forum_id']) - 1;
+		$url = $this->generate_topic_link($event['row']['forum_id'], $event['row']['topic_id']);
+
+		$tpl_ary = $event['tpl_ary'];
+		$tpl_ary['U_LAST_POST'] = $this->generate_lastpost($replies, $url) . '#p' . $event['row']['topic_last_post_id'];
+		$event['tpl_ary'] = $tpl_ary;
+	}
+
+	/**
+	 * Rewrite links to topics in forum view
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function viewforum_modify_topicrow($event)
+	{
+		$topic_row = $event['topic_row'];
+		$topic_row['U_LAST_POST'] = $this->generate_lastpost($event['topic_row']['REPLIES'], $topic_row['U_VIEW_TOPIC']) . '#p' . $event['row']['topic_last_post_id'];
+		$event['topic_row'] = $topic_row;
+	}
+
+	/**
+	 * Rewrite mini post img link
+	 *
+	 * @param	object	$event	The event object
+	 * @return	null
+	 * @access	public
+	 */
+	public function viewtopic_modify_post_row($event)
+	{
+		$row = $event['post_row'];
+		$row['U_MINI_POST'] = $this->generate_topic_link($event['topic_data']['forum_id'], $event['topic_data']['topic_id']) . '#p' . $event['row']['post_id'];
+		$event['post_row'] = $row;
+	}
+
+	/**
+	 * Generate link to topic
+	 *
+	 * @param int $forum_id
+	 * @param int $topic_id
+	 * @return string
+	 */
+	private function generate_topic_link($forum_id, $topic_id)
+	{
+		$start = $this->request->variable('start', 0);
+		return $this->phpbb_root_path . 'viewtopic.' . $this->php_ext . '?f=' . $forum_id . '&t=' . $topic_id . (($start > 0) ? '&start=' . $start : '');
+	}
+
+	/**
+	 * Generate link to last post
+	 *
+	 * @global $_SID		string
+	 * @param $replies		int			Replays in the topic
+	 * @param $url		string		URL oft the topic
+	 * @return			string		The URL with start included
+	 */
+	private function generate_lastpost($replies, $url)
+	{
+		$per_page = ($this->config['posts_per_page'] <= 0) ? 1 : $this->config['posts_per_page'];
+		if (($replies + 1) > $per_page)
+		{
+			$times = 1;
+			for ($j = 0; $j < $replies + 1; $j += $per_page)
+			{
+				$last_post_link = append_sid($url .  '&start=' . $j);
+				$times++;
+			}
+		}
+		else
+		{
+			$last_post_link = $url;
+		}
+		return $last_post_link;
+	}
+
+	/**
+	 * Get the topics post count or the forums post/topic count based on permissions
+	 *
+	 * @param $mode		string	One of topic_posts, forum_posts or forum_topics
+	 * @param $data		array		Array with the topic/forum data to calculate from
+	 * @param $forum_id	int		The forum id is used for permission checks
+	 * @return			int		Number of posts/topics the user can see in the topic/forum
+	 */
+	private function get_count($mode, $data, $forum_id)
+	{
+		if (!$this->auth->acl_get('m_approve', $forum_id))
+		{
+			return (int) $data[$mode . '_approved'];
+		}
+
+		return (int) $data[$mode . '_approved'] + (int) $data[$mode . '_unapproved'] + (int) $data[$mode . '_softdeleted'];
+	}
+
+}
